29594bbeea44338f4e259c80a64065732a9007bc
thiago00dudu-lab <thiago00dudu@gmail.com>
2026-03-13 04:38:46 -0300

    Create pix.js

---
+++ api/pix.js
@@ -0,0 +1,30 @@
+// api/pix.js
+export default async function handler(req, res) {
+    const token = process.env.MERCADO_PAGO_ACCESS_TOKEN; // Aqui ele pega a chave que você salvou na Vercel!
+
+    if (req.method === 'POST') {
+        const { valor, usuario } = req.body;
+        
+        try {
+            const response = await fetch('https://api.mercadopago.com/v1/payments', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                    'X-Idempotency-Key': Math.random().toString(36)
+                },
+                body: JSON.stringify({
+                    transaction_amount: parseFloat(valor),
+                    description: `Deposito ICE - ${usuario}`,
+                    payment_method_id: "pix",
+                    payer: { email: `${usuario}@ice.com` }
+                })
+            });
+
+            const data = await response.json();
+            return res.status(200).json(data);
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro ao processar Pix' });
+        }
+    }
+}
